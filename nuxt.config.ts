d62a252f38f12fdec216fab7cdc35d2bdfd1d793
CN059 <cn059@outlook.com>
2026-02-20 23:17:43 +0800

    feat: 添加i18n配置
    请注意,现在请在en文件夹编写中文文档,zh-CN编写英文.因为路径存在bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
+  modules: ["@nuxtjs/i18n"],
   i18n: {
-    defaultLocale: 'en',
-    locales: [{
-      code: 'en',
-      name: 'English',
-    }, {
-      code: 'fr',
-      name: 'Français',
-    }],
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+      },
+      {
+        code: "fr",
+        name: "Français",
+      },
+      { code: "zh-CN", name: "简体中文" },
+    ],
   },
-})
+});
